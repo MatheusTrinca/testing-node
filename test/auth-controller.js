2c3d60cc541b4f3728bd066c849bf6aa9d5cc3e3
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-08-24 21:28:45 -0300

    feed tests

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,18 +1,43 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const AuthController = require('../controllers/auth');
-const User = require('../models/user');
 const mongoose = require('mongoose');
 
-describe('Auth Controller - Login', () => {
-  it('Should throws an error with status code 500 if access database fails', done => {
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller', function () {
+  before(function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://matheus:nKaRQ9yS7flsWZV8@cluster0.9nli6.mongodb.net/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a',
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function () {});
+
+  afterEach(function () {});
+
+  it('should throw an error with code 500 if accessing the database fails', function (done) {
     sinon.stub(User, 'findOne');
     User.findOne.throws();
 
-    req = {
+    const req = {
       body: {
-        email: 'test@email.com',
-        password: 'test123',
+        email: 'test@test.com',
+        password: 'tester',
       },
     };
 
@@ -25,48 +50,33 @@ describe('Auth Controller - Login', () => {
     User.findOne.restore();
   });
 
-  it('Should send a message with a valid user status for an existing user', done => {
-    mongoose
-      .connect(
-        'mongodb+srv://matheus:nKaRQ9yS7flsWZV8@cluster0.9nli6.mongodb.net/test-messages?retryWrites=true&w=majority',
-        {
-          useNewUrlParser: false,
-          useUnifiedTopology: true,
-          useFindAndModify: false,
-        }
-      )
-      .then(result => {
-        const user = new User({
-          name: 'Matheus',
-          email: 'test@test.com',
-          password: 'test123',
-          posts: [],
-        });
-        return user.save();
+  it('should send a response with a valid user status for an existing user', function (done) {
+    const req = { userId: '5c0f66b979af55031b34728a' };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal('I am new!');
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
       })
-      .then(user => {
-        const req = { userId: user._id };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function (data) {
-            this.userStatus = data.status;
-          },
-        };
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal('I am new!');
-          User.deleteMany({}).then(() => {
-            return mongoose.disconnect().then(() => {
-              done();
-            });
-          });
-        });
-      })
-      .catch(err => console.log(err));
+      .then(() => {
+        done();
+      });
   });
 });
---
+++ test/feed-controller.js
@@ -0,0 +1,68 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+const FeedController = require('../controllers/feed');
+
+describe('Feed Controller', function () {
+  before(function (done) {
+    mongoose
+      .connect(
+        'mongodb+srv://matheus:nKaRQ9yS7flsWZV8@cluster0.9nli6.mongodb.net/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a',
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  beforeEach(function () {});
+
+  afterEach(function () {});
+
+  it('should add a new post to the posts of creator', function (done) {
+    const req = {
+      body: {
+        title: 'Post',
+        content: 'A content',
+      },
+      file: {
+        path: 'abc',
+      },
+      userId: '5c0f66b979af55031b34728a',
+    };
+
+    const res = {
+      status: function () {
+        return this;
+      },
+      json: function () {},
+    };
+
+    FeedController.createPost(req, res, () => {}).then(savedUser => {
+      expect(savedUser).to.have.property('posts');
+      expect(savedUser.posts).to.have.length(1);
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
+  });
+});
